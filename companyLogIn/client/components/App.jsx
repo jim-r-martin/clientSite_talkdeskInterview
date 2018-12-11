1da5304caa66e1f229b3368f65336f03df83de80
jimMartin4321 <jamesrm4321@gmail.com>
2018-12-11 13:46:42 -0800

    added files for companyLogIn and UserLogIn so that different url's can render different pages and we only need one repo to server different static folders

--- companyLogIn/client/components/App.jsx
+++ companyLogIn/client/components/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Header from './Header';
+import CompanyDomainForm from './CompanyDomainForm';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      uiState: 0,
+    };
   }
 
   render() {
+    const { uiState } = this.state;
     return (
     <div>
       <Header />
+      <CompanyDomainForm />
     </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const path = require('path');
 
 const app = express();
 const port = 3004;
-const public_src = path.join(__dirname, '../public');
+const public_companyLogIn_src = path.join(__dirname, '../companyLogIn/public');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
-app.use(express.static(public_src));
+
+app.use('/users/sign_in', express.static(public_companyLogIn_src));
 
 app.listen(port, (err) => {
   if (err){
