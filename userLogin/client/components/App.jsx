e10a749c0db66dfd1cdf0856f2f74d1e33e8556c
jimMartin4321 <jamesrm4321@gmail.com>
2018-12-11 16:41:09 -0800

    mvp for user login page

--- userLogin/client/components/App.jsx
+++ userLogin/client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import UserLoginForm from './UserLoginForm';
 import photo from '../../public/talkdeskMarketingPhoto.png';
 
 const App = () => (
   <div className="wrapper">
     <div className="loginBody">
-      <Header />
+      <div className="loginWrapper">
+        <Header />
+        <UserLoginForm />
+      </div>
     </div>
     <div className="marketingPhoto">
     </div>
