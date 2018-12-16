495b30fc1ea0b7b85e466312fd6500701c471551
jimMartin4321 <jamesrm4321@gmail.com>
2018-12-15 16:32:32 -0800

    changed root component of help request to contain state for all of the form items and updated properly on any input change

--- helpRequest/client/components/App.jsx
+++ helpRequest/client/components/App.jsx
@@ -2,12 +2,39 @@ import React from 'react';
 import RequestForm from './RequestForm';
 import Header from './Header';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      phone: '+1',
+      subject: '',
+      description: '',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-const App = () => (
-  <div className="root">
-    <Header />
-    <RequestForm />
-  </div>
-);
+  onChange(event) {
+    const { value } = event.target;
+    const propertyName = event.target.className;
+    this.setState({
+      [propertyName]: value,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+    <div className="root">
+      <Header />
+      <RequestForm onChange={this.onChange} onSubmit={this.onSubmit} />
+    </div>
+    );
+  }
+}
 
 export default App;
--- helpRequest/client/components/RequestForm.jsx
+++ helpRequest/client/components/RequestForm.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import InputFieldTemplate from './InputFieldTemplate';
 
 const RequestForm = (props) => {
-
+  const { onChange, onSubmit } = props;
   return (
     <div className="requestForm">
       <div className="formTitle"><h3>Submit a request</h3></div>
       <div className="formContainer">
-        <InputFieldTemplate label={'PhoneNumber'} className={'phoneNumber'} />
-        <InputFieldTemplate label={'Subject'} className={'subject'} />
-        <InputFieldTemplate label={'Description'} className={'description'} />
-        <button>Submit</button>
+        <InputFieldTemplate label={'Name'} className={'name'} onChange={onChange} />
+        <InputFieldTemplate label={'Phone Number'} className={'phone'} onChange={onChange} />
+        <InputFieldTemplate label={'Subject'} className={'subject'} onChange={onChange} />
+        <InputFieldTemplate label={'Description'} className={'description'} onChange={onChange} />
+        <button type="button" onClick={onSubmit}>Submit</button>
       </div>
     </div>
   );
